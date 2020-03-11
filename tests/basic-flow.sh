728d81471541c703a9e986865709c575f2a2709d
Staffan Olsson <staffan@repos.se>
2020-03-11 16:06:06 +0100

    Shows that we fail to copy metadata

--- tests/basic-flow.sh
+++ tests/basic-flow.sh
@@ -11,7 +11,8 @@ until mc --no-color config host add minio0 http://minio0:9000 $MINIO_ACCESS_KEY 
 mc --no-color mb minio0/bucket.write
 
 echo "Added before watch" > test1.txt
-mc --no-color cp test1.txt minio0/bucket.write/
+mc --no-color cp --attr "Content-Type=text/testing1" test1.txt minio0/bucket.write/
+mc --no-color stat --json minio0/bucket.write/test1.txt | grep '"Content-Type":"text/testing1"'
 
 mc --no-color mb minio0/bucket.read
 
@@ -19,12 +20,13 @@ expected=minio0/bucket.read/$(sha256sum test1.txt | cut -d' ' -f1).txt
 retrywait=0
 until mc --no-color stat "$expected"; \
   do [ $(( retrywait++ )) -lt 10 ]; sleep 1; done
+mc --no-color stat --json "$expected" | grep '"Content-Type":"text/testing1"'
 
 mc --no-color ls minio0/bucket.write
 mc --no-color ls minio0/bucket.read
 
 echo "Added after watch start" > test2.txt
-mc --no-color cp test2.txt minio0/bucket.write/
+mc --no-color cp --attr "Content-Type=text/testing2" test2.txt minio0/bucket.write/
 
 expected=minio0/bucket.read/$(sha256sum test2.txt | cut -d' ' -f1).txt
 retrywait=0
