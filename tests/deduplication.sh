7705811561517ad0029aa806c2838043e47763f8
Staffan Olsson <staffan@repos.se>
2020-03-31 05:14:05 +0200

    Speed up the check for new meta

--- tests/deduplication.sh
+++ tests/deduplication.sh
@@ -18,15 +18,13 @@ curl -f -v --retry 3 -T "$name" \
   -H 'x-amz-meta-revision: My Identical' \
   "http://minio0:9000/bucket.write/$name"
 
-# TODO clarify that with a different extension there will be a new file even if content is identical
-# ... but ... lowercakse? jpg/jpeg?
-
-# TODO do we expect metadata to be updated?
-for i in 1 2 3; do
-  curl -f -v -I -s http://minio0:9000/bucket.read/$dir$hash.svg 2>&1 \
-    | grep 'X-Amz-Meta-Revision:\|Last-Modified:'
-  sleep 1
+cat "$name.1.headers" | grep 'Last-Modified:'
+for i in 1 2 3 4 5; do
+  curl -f -I -s http://minio0:9000/bucket.read/$dir$hash.svg | tee "$name.2.headers"
+  grep 'My Identical' "$name.2.headers" && break
 done
+cat "$name.2.headers" | grep 'X-Amz-Meta-Revision: My Identical'
+cat "$name.2.headers" | grep 'Last-Modified:'
 
 echo "<svg />" > "$name"
 curl -f -v --retry 3 -T "$name" \
