f116283d7878c51425b14a2193e98cedf153ed37
Staffan Olsson <staffan@repos.se>
2020-08-21 09:26:15 +0200

    With cleanup introduced tests can no longer verify inbox objects

--- tests/custom-metadata.sh
+++ tests/custom-metadata.sh
@@ -9,9 +9,9 @@ curl -f -v --retry 3 -T "$name" \
   -H 'x-amz-meta-my-caption: Amazing story by me' \
   "http://minio0:9000/bucket.write/$name"
 
-mc --no-color stat --json minio0/bucket.write/$name | grep '"X-Amz-Meta-My-Caption":"Amazing story by me"'
-
 hash=$(sha256sum "$name" | cut -d' ' -f1)
 dir=${hash:0:2}/${hash:2:2}/
+mc --no-color stat --json minio0/bucket.read/$dir$hash.md | jq '.'
+[ "$(mc --no-color stat --json minio0/bucket.read/$dir$hash.md  | jq -r '.metadata."X-Amz-Meta-My-Caption"')" = "Amazing story by me" ]
 curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.md | tee "$name.headers"
 cat "$name.headers" | grep 'x-amz-meta-my-caption: Amazing story by me'
