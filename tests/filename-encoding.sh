0050659e98db3a69f7961354356ac6ff2a4bce4e
Staffan Olsson <staffan@repos.se>
2021-02-23 10:44:08 +0100

    curl retries won't help when expecting an update to an existing blob

--- tests/filename-encoding.sh
+++ tests/filename-encoding.sh
@@ -16,21 +16,25 @@ cat "$name.headers1" | grep 'Content-Disposition' | grep 'filename=with#bracket.
 
 curl -f -v --retry 3 -T "$name" \
   "http://minio0:9000/bucket.write/with%25percent.txt"
+sleep 3
 curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers2"
 diff -u "$name.headers1" "$name.headers2"
 cat "$name.headers2" | grep 'Content-Disposition' | grep 'filename=with%percent.txt'
 
 curl -f -v --retry 3 -T "$name" \
   "http://minio0:9000/bucket.write/with%22quote.txt"
+sleep 3
 curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers3"
 cat "$name.headers3" | grep 'Content-Disposition' | grep 'filename=with"quote.txt'
 
 curl -f -v --retry 3 -T "$name" \
-  "http://minio0:9000/bucket.write/with'singlequote.txt"
+  "http://minio0:9000/bucket.write/with%27singlequote.txt"
+sleep 3
 curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers4"
 cat "$name.headers4" | grep 'Content-Disposition' | grep "filename=with'singlequote.txt"
 
 curl -f -v --retry 3 -T "$name" \
-  "http://minio0:9000/bucket.write/with;semicolon.txt"
+  "http://minio0:9000/bucket.write/with%3Bsemicolon.txt"
+sleep 3
 curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers5"
 cat "$name.headers5" | grep 'Content-Disposition' | grep "filename=with;semicolon.txt"
