e85708fd15465e8d9465929153fc288db4d4ffd4
Staffan Olsson <staffan@repos.se>
2021-02-23 10:02:31 +0100

    Make sure we run latest minio-go v6; go mod tidy

--- tests/filename-encoding.sh
+++ tests/filename-encoding.sh
@@ -11,8 +11,26 @@ curl -f -v --retry 3 -T "$name" \
 hash=$(sha256sum "$name" | cut -d' ' -f1)
 dir=${hash:0:2}/${hash:2:2}/
 mc --no-color stat --json minio0/bucket.read/$dir$hash.txt | jq '.'
-curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers"
-cat "$name.headers" | grep 'Content-Disposition' | grep 'filename=with#bracket.txt'
+curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers1"
+cat "$name.headers1" | grep 'Content-Disposition' | grep 'filename=with#bracket.txt'
 
 curl -f -v --retry 3 -T "$name" \
   "http://minio0:9000/bucket.write/with%25percent.txt"
+curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers2"
+diff -u "$name.headers1" "$name.headers2"
+cat "$name.headers2" | grep 'Content-Disposition' | grep 'filename=with%percent.txt'
+
+curl -f -v --retry 3 -T "$name" \
+  "http://minio0:9000/bucket.write/with%22quote.txt"
+curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers3"
+cat "$name.headers3" | grep 'Content-Disposition' | grep 'filename=with"quote.txt'
+
+curl -f -v --retry 3 -T "$name" \
+  "http://minio0:9000/bucket.write/with'singlequote.txt"
+curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers4"
+cat "$name.headers4" | grep 'Content-Disposition' | grep "filename=with'singlequote.txt"
+
+curl -f -v --retry 3 -T "$name" \
+  "http://minio0:9000/bucket.write/with;semicolon.txt"
+curl -f -v --retry 3 -I http://minio0:9000/bucket.read/$dir$hash.txt | tee "$name.headers5"
+cat "$name.headers5" | grep 'Content-Disposition' | grep "filename=with;semicolon.txt"
