359f0eb7d89bd5cf274ae4e0a5ae6e7bf88dd33e
Staffan Olsson <staffan@repos.se>
2023-01-07 07:57:41 +0100

    A way to run the same tests using different stacks

--- test.sh
+++ test.sh
@@ -4,14 +4,16 @@ echo "DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM"
 
 #  Note that to troubleshoot it's better to run docker-compose -f docker-compose.test.yml up
 
-# What we think Docker Hub is running
-TESTS_DISABLED=false docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from sut sut
-RESULT=$?
-
-echo "Other logs:"
-docker-compose -f docker-compose.test.yml logs --no-color | grep -v '^sut_'
-
-docker-compose -f docker-compose.test.yml down --remove-orphans -v
-
-echo "Result: $RESULT"
-exit $RESULT
+for TEST in \
+    "-f docker-compose.test.yml" \
+    "-f docker-compose.test.yml -f docker-compose.test-kafka.yml" \
+    ; do
+  echo "=> $TEST"
+  TESTS_DISABLED=false docker-compose $TEST up --build --abort-on-container-exit --exit-code-from sut sut
+  RESULT=$?
+  echo "=> Logs from the other containers:"
+  docker-compose $TEST logs --no-color | grep -v 'sut-1[[:space:]]*|'
+  echo "=> Exit $RESULT for $TEST"
+  docker-compose $TEST down --remove-orphans -v
+  [ $RESULT -eq 0 ] || exit $RESULT 
+done 
