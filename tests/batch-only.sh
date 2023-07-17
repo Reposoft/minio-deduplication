0ae85dad56c6e1d98d0b9e5a8bd62228d1af58da
Staffan Olsson <staffan@repos.se>
2023-07-17 07:08:41 +0200

    Only the first part of basic-flow is applicable to batch mode

--- tests/batch-only.sh
+++ tests/batch-only.sh
@@ -16,7 +16,37 @@ trap 'onerr $LINENO' ERR
 echo "_____ batch test start _____"
 sleep 1
 
-basic-flow.sh
+[ -n "$RETRIES" ] || RETRIES=3
+
+curl -f --retry 3 --retry-connrefused http://app0:2112/metrics > /dev/null
+
+retrywait=0
+until mc --no-color config host add minio0 http://minio0:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY; \
+  do [ $(( retrywait++ )) -lt 30 ]; sleep 1; done
+
+mc --no-color mb minio0/bucket.write
+
+mc event add minio0/bucket.write arn:minio:sqs::_:kafka --event put || \
+  [ -z "$REQUIRE_KAFKA" ] || exit 1
+
+echo "Added before watch" > test1.txt
+mc --no-color cp --attr "Content-Type=text/testing1" test1.txt minio0/bucket.write/
+mc --no-color stat --json minio0/bucket.write/test1.txt | grep '"Content-Type":"text/testing1"'
+
+mc --no-color mb minio0/bucket.read
+
+hash=$(sha256sum test1.txt | cut -d' ' -f1)
+dir=${hash:0:2}/${hash:2:2}/
+expected=minio0/bucket.read/$dir$hash.txt
+retrywait=0
+until mc --no-color stat "$expected"; \
+  do [ $(( retrywait++ )) -lt $RETRIES ]; sleep $ACCEPTABLE_TRANSFER_DELAY; done
+mc --no-color stat --json "$expected" | grep '"Content-Type":"text/testing1"'
+
+mc --no-color ls minio0/bucket.write
+mc --no-color ls minio0/bucket.read
+
+curl -s http://app0:2112/metrics | tee metrics.txt | grep blobs_
 
 echo "_____ batch test executed _____"
 after-all.sh
