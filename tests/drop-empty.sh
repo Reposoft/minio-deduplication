5de81761935370f0f8d8258f04899820438a01e6
Staffan Olsson <staffan@repos.se>
2023-10-21 07:19:07 +0200

    Failed to put an actual empty file with curl, got status 400

--- tests/drop-empty.sh
+++ tests/drop-empty.sh
@@ -2,14 +2,13 @@
 set -eo pipefail
 [ -z "$DEBUG" ] || set -x
 
-name="Some random file.txt"
-echo -n "" > "$name"
-
-curl -f -v --retry 3 -T "$name" \
+curl -f -v --retry 3 -X PUT \
+  -H "Content-Type: text/plain" \
   -H 'x-amz-meta-my-test: My meta' \
-  "http://minio0:9000/bucket.write/original/dir/$name"
+  "http://minio0:9000/bucket.write/original/dir/somefile.txt"
 sleep $ACCEPTABLE_TRANSFER_DELAY
-hash=$(sha256sum "$name" | cut -d' ' -f1)
-[ $hash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" ] || exit 1
+hash="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 dir=${hash:0:2}/${hash:2:2}/
 ! mc --no-color stat --json minio0/bucket.read/$dir$hash.txt 2>/dev/null || false
+
+# obviously hard to validate in an integration test, but look for "Dropped empty file" in app logs
